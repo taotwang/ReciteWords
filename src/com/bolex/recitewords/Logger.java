77d8ab907bdf7283fbc42f29182095d3304856b7
taotwang <1040556070@qq.com>
2017-10-30 14:26:58 +0800

    Revert "Revert "Revert "test"""
    This reverts commit e4f55fda7d65c8d0c2ac1983305a19d3e2fc1590.

--- src/com/bolex/recitewords/Logger.java
+++ src/com/bolex/recitewords/Logger.java
@@ -7,8 +7,6 @@ import com.intellij.notification.*;
  * Created by moxun on 15/11/27.
  */
 public class Logger {
-
-	//add by wangtao for test
     private static String NAME;
     private static int LEVEL = 0;
 
